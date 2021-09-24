679bba11927349bd19d828f705f7c7581c2435b7
l-gorman <lgorman@turing.ac.uk>
2021-09-24 15:01:00 +0000

    commit local changes

--- routes/auth.js
+++ routes/auth.js
@@ -43,8 +43,8 @@ router.post('/register', async (req, res) => {
     console.log(centralResultUsers.config)
 
     const user = centralResultUsers.data.filter(user => user.email === req.body.email)
-    if (user.length > 1) res.status(400).send("multiple users with that email in central database")
-    if (user.length === 1) res.status(400).send("A user with that email already exists in ODK central database")
+    if (user.length > 1) res.send("multiple users with that email in central database")
+    if (user.length === 1) res.send("A user with that email already exists in ODK central database")
 
 
     // Obtaining central access token
