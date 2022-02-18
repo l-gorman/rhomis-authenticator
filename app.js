32cf6f2c9be25493b78985f1d657d29ffb38202d
l-gorman <lgorman@turing.ac.uk>
2022-02-18 12:03:01 +0000

    removing email transporter from main app to stop test crash

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ dotenv.config()
 
 const mongoose = require('mongoose')
 
-CreateTransporter = require('./email-transporter/email-transporter')
-CreateTransporter()
+
 
 // console.log(process.env.RECAPTCHA_SECRET_KEY)
 
@@ -19,27 +18,6 @@ const metaDataRoute = require('./routes/metaData')
 const adminRoute = require('./routes/makeAdmin').router
 const testEmailRoute = require('./routes/email-test')
 
-
-
-
-
-
-// transporter.verify(function (error, success) {
-//     if (error) {
-//         console.log("transporter error")
-//         console.log(error)
-//     } else {
-//         console.log("Server ready for messages")
-//     }
-// })
-
-// transporter.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//         return console.log(error);
-//     }
-
-//     console.log('Message sent: ' + info.response);
-// });
 
 // Rate limiting
 const rateLimit = require("express-rate-limit");
